180331281d5647cbc62e8383d19cdb841935ad3e
Danil Zobkov <56187773+Owelve@users.noreply.github.com>
2024-02-07 18:44:00 +0300

    added dark theme

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const name = document.getElementById("name-id");
 const labelContainer = document.getElementById('labelContainer');
+let switchMode = document.getElementById('themeSwitch');
 let labelCount = 0;
+
+// Смена темы
+
+switchMode.onclick = function () {
+    let theme = document.getElementById('theme');
+
+    if (theme.getAttribute("href") == "css/theme-light.css")
+        theme.href = "css/theme-dark.css";
+    else
+        theme.href = "css/theme-light.css";
+};
+
+
+// Проверка на тип
 
 function validName() {
     if (isNaN(name.value.charAt(0)))
@@ -10,6 +25,8 @@ function validName() {
         alert('Введи текст епта');
     
 };
+
+// Создание рандомного label
 
 function createRandomLabel() {
     if (labelCount >= 3) {
